72809347854621c5c4e213b8ca17771c365e03b4
Jonathan <jtc7537@gmail.com>
2019-03-23 14:32:53 -0400

    scaffolding

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sandbox></Sandbox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sandbox from './components/Sandbox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sandbox
   }
 }
 </script>
