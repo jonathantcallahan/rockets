ed17014ad3c1c5c35568369b66dd44f8794f87b7
Jonathan <jtc7537@gmail.com>
2019-03-28 21:54:46 -0400

    hoops

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <Sandbox></Sandbox>
-  </div>
 </template>
 
 <script>
@@ -17,12 +14,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0px;
+  margin: 0px
 }
 </style>
--- client/src/components/Sandbox.vue
+++ client/src/components/Sandbox.vue
@@ -1,8 +1,58 @@
  <template>
+    <div class='game-container'>
+        <div 
+            v-for="(hoop,i) in hoopLocations" 
+            :key="'hoop-' + i" 
+            class='hoop'
+            :style="{top:hoop[1] + 'px', left:hoop[0] + 'px'}">{{hoop[1]}}</div>
+    </div>
  </template>
 
  <script>
  export default {
-     name: 'Sandbox'
+     name: 'Sandbox',
+     data: function () {
+         return {
+             rocketLocation: [],
+             hoopLocations: []
+         }
+     },
+     methods: {
+         createHoops: function () {
+             const xLoc = Math.floor(Math.random() * window.innerWidth)
+             this.hoopLocations.push([xLoc, 0])
+             setTimeout(this.createHoops, 2000)
+         },
+         moveHoops: function () {
+             this.hoopLocations.forEach((e,i) => {
+                 this.hoopLocations[i][1] = this.hoopLocations[i][1] + 5
+                 if(e[1] > window.innerHeight - 10) {
+                     this.hoopLocations.splice(i,1)
+                 }
+             })
+             this.$forceUpdate()
+             setTimeout(this.moveHoops, 50)
+         }
+     },
+     created () {
+         this.createHoops()
+         this.moveHoops()
+     }
  }
  </script>
+
+ <style>
+ .game-container {
+     width: 100vw;
+     height: 100vh;
+     background-color: gray;
+     position: relative
+ }
+
+ .hoop {
+     width: 200px;
+     height: 5px;
+     background-color: black;
+     position: absolute
+ }
+ </style>
