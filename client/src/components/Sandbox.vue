83b267b9e4f6d9a415c13e5e5f116ddd68032a01
Jonathan <jtc7537@gmail.com>
2019-03-30 11:09:14 -0400

    points

--- client/src/components/Sandbox.vue
+++ client/src/components/Sandbox.vue
@@ -1,12 +1,18 @@
  <template>
     <div class='game-container'>
+        <div class='score-container'>
+            <div class='points'>{{this.points}}</div>
+        </div>
         <div 
             v-for="(hoop,i) in hoopLocations" 
             :key="'hoop-' + i" 
             class='hoop'
             :style="{top:hoop[1] + 'px', left:hoop[0] + 'px'}">{{hoop[1]}}
         </div>
-        <div class='rocket'></div>
+        <div 
+            class='rocket'
+            :style="{ top:rocketLocation[1] + 'px', left:rocketLocation[0] + 'px' }">
+        </div>
     </div>
  </template>
 
@@ -15,19 +21,23 @@
      name: 'Sandbox',
      data: function () {
          return {
-             rocketLocation: [],
-             hoopLocations: []
+             rocketLocation: [0,window.innerHeight/2],
+             hoopLocations: [],
+             points: 0
          }
      },
      methods: {
          createHoops: function () {
              const xLoc = Math.floor(Math.random() * window.innerWidth)
              this.hoopLocations.push([xLoc, 0])
-             setTimeout(this.createHoops, 2000)
+             setTimeout(this.createHoops, 4000)
          },
          moveHoops: function () {
              this.hoopLocations.forEach((e,i) => {
                  this.hoopLocations[i][1] = this.hoopLocations[i][1] + 5
+                 const xLoc = this.rocketLocation[0] - this.hoopLocations[i][0]
+                 console.log(xLoc)
+                 if(Math.abs(this.hoopLocations[i][1] - this.rocketLocation[1]) < 20 && (xLoc < 200 && xLoc > 0)){ this.points = this.points + 1}
                  if(e[1] > window.innerHeight - 10) {
                      this.hoopLocations.splice(i,1)
                  }
@@ -37,6 +47,8 @@
          },
          moveRocket: function (keycode) {
              console.log(keycode)
+             if(keycode == 'ArrowLeft' && this.rocketLocation[0] > 0){ this.rocketLocation[0] = this.rocketLocation[0] - 30 }
+             if(keycode == 'ArrowRight' && this.rocketLocation[0] < window.innerWidth - 10){ this.rocketLocation[0] = this.rocketLocation[0] + 30 }
          }
      },
      created () {
@@ -62,5 +74,18 @@
      height: 5px;
      background-color: black;
      position: absolute
+ }
+
+ .rocket {
+     width: 20px;
+     height: 100px;
+     position: absolute;
+     background-color: white
+ }
+
+ .score-container {
+     position: absolute;
+     top: 10px;
+     left: 10px
  }
  </style>
